d1d100ebfac7b7afe616e929d0d8ceaf8f65cc94
abwaris7 <hellokhan.ch@gmail.com>
2025-06-03 11:11:46 +0200

    Open the control panel

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+
+        System.out.println("Welcome to the traffic management system!");
+
+        System.out.println("1. Add");
+        System.out.println("2. Delete");
+        System.out.println("3. System");
+        System.out.println("0. Quit");
     }
 }
