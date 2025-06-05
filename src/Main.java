de58bee1d8f3a20eaf2260a22bc1afb17278dc65
abwaris7 <hellokhan.ch@gmail.com>
2025-06-05 18:02:05 +0200

    bug fixed

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the traffic management system!");
 
-        System.out.print("Input the number of roads:");
-        int roadNumber = readValidNumber(scanner);
+        System.out.print("Input the number of roads: ");
+        int roadNumber = readValidValue(scanner);
 
-        System.out.print("Input the interval:");
-        int intervalNumber = readValidNumber(scanner);
+        System.out.print("Input the interval: ");
+        int intervalNumber = readValidValue(scanner);
 
+        menu();
         int option;
         do {
-            menu();
             option = readValidMenuOption(scanner);
+
             switch (option) {
                 case 1:
                     System.out.println("Road added");
@@ -28,8 +29,10 @@ public class Main {
                     break;
                 case 0:
                     System.out.println("Bye!");
-                    break;
+                    System.exit(1);
             }
+
+
         } while (option != 0);
 
         scanner.close();
@@ -39,7 +42,7 @@ public class Main {
         System.out.println("Menu:");
         System.out.println("1. Add road");
         System.out.println("2. Delete road");
-        System.out.println("3. Open System");
+        System.out.println("3. Open system");
         System.out.println("0. Quit");
     }
 
@@ -52,6 +55,22 @@ public class Main {
                 if (number >= 0 && number <= 3) {
                     return number;
                 } else {
+                    System.out.println("Incorrect option ");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Incorrect option");
+            }
+        }
+    }
+
+    public static int readValidValue(Scanner scanner) {
+        while (true) {
+            String input = scanner.next();
+            try {
+                int number = Integer.parseInt(input);
+                if (number > 0 && number <= 5) {
+                    return number;
+                } else {
                     System.out.print("Error! Incorrect input! Try again: ");
                 }
             } catch (NumberFormatException e) {
@@ -62,7 +81,6 @@ public class Main {
 
     // Specifically for menu options
     public static int readValidMenuOption(Scanner scanner) {
-        //System.out.print("Choose an option (0-3): ");
         return readValidNumber(scanner);
     }
 }
